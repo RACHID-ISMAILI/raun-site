81aa1e0e893b48c881d1952437c6a26090ceda23
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-25 21:39:16 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,11 +1,12 @@
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// firebaseConfig.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD0R0IFgjCk3gWgVxK3-WnfLubhAqsKbOM",
   authDomain: "raun-network.firebaseapp.com",
   projectId: "raun-network",
-  storageBucket: "raun-network.firebasestorage.app",
+  storageBucket: "raun-network.appspot.com",
   messagingSenderId: "541416001018",
   appId: "1:541416001018:web:df564a55255d4615206843",
   measurementId: "G-LVV48NKDF8"
@@ -13,3 +14,5 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+export { app };
