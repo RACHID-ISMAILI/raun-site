94619b0226a9292655d2c5822d82817e94d657c2
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-25 22:01:01 +0100

    Add files via upload
    firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,19 +1,22 @@
 
-// firebaseConfig.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
-import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-analytics.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyD0R0IFgjCk3gWgVxK3-WnfLubhAqsKbOM",
   authDomain: "raun-network.firebaseapp.com",
   projectId: "raun-network",
-  storageBucket: "raun-network.appspot.com",
+  storageBucket: "raun-network.firebasestorage.app",
   messagingSenderId: "541416001018",
   appId: "1:541416001018:web:df564a55255d4615206843",
   measurementId: "G-LVV48NKDF8"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
-export { app };
