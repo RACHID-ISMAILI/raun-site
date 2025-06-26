c495beda5360060864e68a941738cd9959e763d3
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-26 21:58:51 +0100

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,22 +1,19 @@
-
-// Import the functions you need from the SDKs you need
+// Import des fonctions nécessaires
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// Configuration Firebase de ton projet
 const firebaseConfig = {
   apiKey: "AIzaSyD0R0IFgjCk3gWgVxK3-WnfLubhAqsKbOM",
   authDomain: "raun-network.firebaseapp.com",
   projectId: "raun-network",
-  storageBucket: "raun-network.firebasestorage.app",
+  storageBucket: "raun-network.appspot.com",
   messagingSenderId: "541416001018",
   appId: "1:541416001018:web:df564a55255d4615206843",
   measurementId: "G-LVV48NKDF8"
 };
 
-// Initialize Firebase
+// Initialisation Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
