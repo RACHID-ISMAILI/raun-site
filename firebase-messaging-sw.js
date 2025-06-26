07be461ba2887cace8e82f45e97036c11dbbc444
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-26 22:56:32 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,17 +1,17 @@
-// Import Firebase scripts
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging.js');
 
-// Configuration Firebase
 firebase.initializeApp({
   apiKey: "AIzaSyD0R0IFgjCk3gWgVxK3-WnfLubhAqsKbOM",
   authDomain: "raun-network.firebaseapp.com",
   projectId: "raun-network",
   storageBucket: "raun-network.appspot.com",
   messagingSenderId: "541416001018",
-  appId: "1:541416001018:web:df564a55255d4615206843",
-  measurementId: "G-LVV48NKDF8"
+  appId: "1:541416001018:web:df564a55255d4615206843"
 });
 
-// Initialiser Firebase Messaging
 const messaging = firebase.messaging();
+messaging.onBackgroundMessage(payload => {
+  const { title, body } = payload.notification;
+  self.registration.showNotification(title, { body });
+});
