2cf904d6b0ea5c879bf4eab06c84c64fb077e08a
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-27 22:35:54 +0100

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,3 +1,4 @@
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-analytics.js";
