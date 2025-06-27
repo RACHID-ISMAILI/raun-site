5d78fd38995214da350a72d0888d7fb4cd8b0991
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-27 18:34:34 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,14 @@
-// Firebase messaging
+// firebase-messaging-sw.js
+importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/10.12.1/firebase-messaging-compat.js");
+
+firebase.initializeApp({
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-PROJECT.firebaseapp.com",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-PROJECT.appspot.com",
+  messagingSenderId: "SENDER-ID",
+  appId: "APP-ID"
+});
+
+const messaging = firebase.messaging();
