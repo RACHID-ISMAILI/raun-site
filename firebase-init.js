2a99bf68f1a934b911a7c992537639b8d29ba452
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-27 18:33:17 +0100

    Update firebase-init.js

--- firebase-init.js
+++ firebase-init.js
@@ -1,1 +1,16 @@
-// Initialisation Firebase
+// firebase-init.js
+// Import the functions you need from the SDKs
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "YOUR-API-KEY",
+  authDomain: "YOUR-PROJECT.firebaseapp.com",
+  projectId: "YOUR-PROJECT-ID",
+  storageBucket: "YOUR-PROJECT.appspot.com",
+  messagingSenderId: "SENDER-ID",
+  appId: "APP-ID"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
