2e0c744b0a614f4f05d307602b9764774b4c617b
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-27 11:49:54 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,22 +1,10 @@
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js');
-
-firebase.initializeApp({
-  apiKey: "AIzaSyD0R0IFgjCk3gWgVxK3-WnfLubhAqsKbOM",
-  authDomain: "raun-network.firebaseapp.com",
-  projectId: "raun-network",
-  storageBucket: "raun-network.appspot.com",
-  messagingSenderId: "541416001018",
-  appId: "1:541416001018:web:df564a55255d4615206843",
-  measurementId: "G-LVV48NKDF8"
-});
-
-const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage(payload => {
-  const { title, body, icon } = payload.notification;
-  self.registration.showNotification(title, {
-    body,
-    icon: icon || '/icon192.png'
-  });
+// Service Worker Firebase pour notifications
+self.addEventListener('push', function(event) {
+  const data = event.data.json();
+  event.waitUntil(
+    self.registration.showNotification(data.title, {
+      body: data.body,
+      icon: '/favicon.ico'
+    })
+  );
 });
