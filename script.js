e4c7b2140c7473af519e46129cfb132b662512ae
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-06-29 15:17:18 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,10 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
+function initLoader() {
+  setTimeout(() => {
+    document.getElementById('loader').style.display = 'none';
+    document.getElementById('main').style.display = 'block';
+  }, 3000); // 3 secondes de chargement
+}
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDvutb_kZGJbjMq4mDzfAEmXsjmSIaUMe4",
-  authDomain: "delgado-nexus-project.firebaseapp.com",
-  projectId: "delgado-nexus-project",
-  storageBucket: "delgado-nexus-project.appspot.com",
-  messagingSenderId: "619502357264",
-  appId: "1:619502357264:web:4ae1f227176eff68bd3395",
-  measurementId: "G-STNQ4SBJGH"
-};
-
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-
-window.saveCapsule = async function (e) {
-  e.preventDefault();
-  const titre = document.getElementById('titre').value;
-  const rappel = document.getElementById('rappel').value;
-  const alignement = document.getElementById('alignement').value;
-  const projection = document.getElementById('projection').value;
-
-  try {
-    await addDoc(collection(db, "capsules"), {
-      titre, rappel, alignement, projection,
-      date: new Date().toISOString()
-    });
-    alert("Capsule enregistrée !");
-  } catch (error) {
-    alert("Erreur: " + error.message);
-  }
+function goToSecret() {
+  window.location.href = "secret.html";
 }
