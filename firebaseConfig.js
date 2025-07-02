ebdeb31f61ce9dbe823e8723d3d5c0ce8e1fa272
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-02 11:39:35 +0100

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,13 +1,3 @@
 
-// Ton fichier Firebase RAUN intégré
-const firebaseConfig = {
-  apiKey: "AIzaSyBzLrEUBrOemOC5exunO1X6UleWlbgA7H0",
-  authDomain: "raun-site.firebaseapp.com",
-  projectId: "raun-site",
-  storageBucket: "raun-site.firebasestorage.app",
-  messagingSenderId: "726026685777",
-  appId: "1:726026685777:web:b29176676060bc12209770"
-};
-
-// Initialisation Firebase
-firebase.initializeApp(firebaseConfig);
+// firebaseConfig.js
+// Utiliser dans une autre étape si besoin
