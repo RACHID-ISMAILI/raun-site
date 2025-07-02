aecfcb8da45c161ecf78ffecb116542e6c6ec402
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-02 10:12:35 +0100

    Add files via upload

--- firebase-init.js
+++ firebase-init.js
@@ -1,18 +1,9 @@
-// Import Firebase modules
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-firestore.js";
-import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/11.9.1/firebase-auth.js";
-
 const firebaseConfig = {
-  apiKey: "AIzaSyDvutb_kZGJbjMq4mDzfAEmXsjmSIaUMe4",
-  authDomain: "delgado-nexus-project.firebaseapp.com",
-  projectId: "delgado-nexus-project",
-  storageBucket: "delgado-nexus-project.appspot.com",
-  messagingSenderId: "619502357264",
-  appId: "1:619502357264:web:4ae1f227176eff68bd3395",
-  measurementId: "G-STNQ4SBJGH"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_STORAGE_BUCKET",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
-
-export const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+firebase.initializeApp(firebaseConfig);
