a9b2e40ead5c116c26f4618a5879367d0e4e1501
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-02 09:02:08 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log('Bienvenue sur le portail sécurisé');
+
+document.getElementById('passwordInput').addEventListener('keyup', function (e) {
+  if (e.key === 'Enter') {
+    const val = e.target.value;
+    if (val === 'globalcode7676') {
+      document.getElementById('adminPanel').style.display = 'block';
+    } else {
+      alert('Mot de passe incorrect');
+    }
+  }
+});
