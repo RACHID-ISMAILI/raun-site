e20f8a03286ef36a679c3e178038978367455ae1
RACHID-ISMAILI <Isra1903@outlook.fr>
2025-07-03 21:48:00 +0100

    Add files via upload

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -1,3 +1,9 @@
-
-// firebaseConfig.js
-// Utiliser dans une autre étape si besoin
+const firebaseConfig = {
+  apiKey: "AIzaSyBzLrEUBrOemOC5exunO1X6UleWlbgA7H0",
+  authDomain: "raun-site.firebaseapp.com",
+  projectId: "raun-site",
+  storageBucket: "raun-site.firebasestorage.app",
+  messagingSenderId: "726026685777",
+  appId: "1:726026685777:web:b29176676060bc12209770"
+};
+firebase.initializeApp(firebaseConfig);
